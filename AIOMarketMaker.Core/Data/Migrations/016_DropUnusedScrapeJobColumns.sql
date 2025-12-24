d55321fd842a679eeb746915a2deff633d486aa7
ryan <ryan75195@gmail.com>
2025-12-24 00:58:53 +0000

    fix: drop default constraints before dropping unused columns

--- AIOMarketMaker.Core/Data/Migrations/016_DropUnusedScrapeJobColumns.sql
+++ AIOMarketMaker.Core/Data/Migrations/016_DropUnusedScrapeJobColumns.sql
@@ -1,8 +1,22 @@
--- Migration: 015_DropUnusedScrapeJobColumns
+-- Migration: 016_DropUnusedScrapeJobColumns
 -- Description: Removes legacy columns from ScrapeJobs that are no longer used
 -- Date: 2025-12-24
 
--- Drop unused columns if they exist
+-- Helper: Drop default constraint on a column before dropping the column
+DECLARE @sql NVARCHAR(MAX);
+
+-- Drop all default constraints on columns we want to remove
+SET @sql = '';
+SELECT @sql = @sql + 'ALTER TABLE ScrapeJobs DROP CONSTRAINT ' + dc.name + ';' + CHAR(13)
+FROM sys.default_constraints dc
+INNER JOIN sys.columns c ON dc.parent_object_id = c.object_id AND dc.parent_column_id = c.column_id
+WHERE dc.parent_object_id = OBJECT_ID('ScrapeJobs')
+AND c.name IN ('BuyingFormat', 'Condition', 'SearchType', 'FrequencyMinutes', 'LookbackDays', 'ItemLimit');
+
+IF @sql <> ''
+    EXEC sp_executesql @sql;
+
+-- Now drop unused columns if they exist
 IF COL_LENGTH('ScrapeJobs', 'BuyingFormat') IS NOT NULL
     ALTER TABLE ScrapeJobs DROP COLUMN BuyingFormat;
 
