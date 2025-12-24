b2359572d0626e264270f219b816cc57cc0901dd
ryan <ryan75195@gmail.com>
2025-12-24 01:21:46 +0000

    fix: add missing using for IConfiguration

--- AIOMarketMaker.Functions/Functions/Diagnostics.cs
+++ AIOMarketMaker.Functions/Functions/Diagnostics.cs
@@ -1,6 +1,7 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using AIOMarketMaker.Core.Data;
